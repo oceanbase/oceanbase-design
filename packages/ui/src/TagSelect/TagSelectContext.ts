b50dcfb72b10c9efe12e64164173e0616371e12e
dengfuping <dengfuping_private@163.com>
2023-09-06 11:06:59 +0800

    fix(ci): test and build site fail

--- packages/ui/src/TagSelect/TagSelectContext.ts
+++ packages/ui/src/TagSelect/TagSelectContext.ts
@@ -1,14 +1,14 @@
-import {createContext} from 'react';
-import type {TagSelectValueType} from './Group';
+import { createContext } from 'react';
+import type { TagSelectValueType } from './Group';
 
 export type TagSelectContextProps = {
-    multiple?: boolean;
-    size?: string;
-    disabled?: boolean,
-    value?: Array<TagSelectValueType>;
-    onChange?: (value: Array<TagSelectValueType> | TagSelectValueType) => void;
-    registerValue?: (val: TagSelectValueType) => void;
-    handleValueChange?: (val: TagSelectValueType) => void;
+  multiple?: boolean;
+  size?: string;
+  disabled?: boolean;
+  value?: Array<TagSelectValueType>;
+  onChange?: (value: Array<TagSelectValueType> | TagSelectValueType) => void;
+  registerValue?: (val: TagSelectValueType) => void;
+  handleValueChange?: (val: TagSelectValueType) => void;
 };
 
 const TagSelectContext = createContext<TagSelectContextProps>({});
