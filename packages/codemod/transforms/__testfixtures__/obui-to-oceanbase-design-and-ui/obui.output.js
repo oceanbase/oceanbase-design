8825f42df7520a5b7f3e5c6552dd068a34fdb205
dengfuping <fuping.dfp@antgroup.com>
2023-09-21 16:37:42 +0800

    fix(codemod): test fail

--- packages/codemod/transforms/__testfixtures__/obui-to-oceanbase-design-and-ui/obui.output.js
+++ packages/codemod/transforms/__testfixtures__/obui-to-oceanbase-design-and-ui/obui.output.js
@@ -5,7 +5,6 @@ import type { BasicLayoutProps } from '@oceanbase/ui/es/BasicLayout';
 import type { LoginProps } from '@oceanbase/ui/es/Login';
 import type { PageContainerProps } from '@oceanbase/ui/es/PageContainer';
 import { Action } from '@alipay/ob-ui-v1';
-import { Action } from '@alipay/ob-ui-v1';
 import type { ActionProps } from '@alipay/ob-ui-v1/es/Action';
 
 const Demo = () => {
