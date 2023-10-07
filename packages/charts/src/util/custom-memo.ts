4f9ea1b352b32556d4a636644f148969b859ff5d
dengfuping <fuping.dfp@antgroup.com>
2023-10-07 16:09:16 +0800

    feat(charts): customMemo support to compare function

--- packages/charts/src/util/custom-memo.ts
+++ packages/charts/src/util/custom-memo.ts
@@ -1,13 +1,28 @@
 import { memo } from 'react';
 import type { FunctionComponent } from 'react';
-import { isEqual } from 'lodash';
+import { isFunction, isEqualWith, isEqual, isPlainObject, maxBy } from 'lodash';
 import type { BaseConfig, AllBaseConfig } from '@ant-design/charts';
+
+export function isEqualWithFunction(v1: any, v2: any) {
+  return isEqualWith(v1, v2, (value1, value2) => {
+    // function equals
+    if (isFunction(value1) && isFunction(value2)) {
+      return value1.toString() === value2.toString();
+    } else if (isPlainObject(value1) && isPlainObject(value2)) {
+      const keys1 = Object.keys(value1);
+      const keys2 = Object.keys(value2);
+      const keys = maxBy([keys1, keys2], o => o.length);
+      return keys.every(key => isEqualWithFunction(value1[key], value2[key]));
+    }
+    return isEqual(value1, value2);
+  });
+}
 
 export function customMemo<P extends BaseConfig<AllBaseConfig>>(
   Component: FunctionComponent<P>,
   propsAreEqual?: (prevProps: Readonly<P>, nextProps: Readonly<P>) => boolean
 ) {
   return memo(Component, (prevProps, nextProps) =>
-    propsAreEqual ? propsAreEqual(prevProps, nextProps) : isEqual(prevProps, nextProps)
+    propsAreEqual ? propsAreEqual(prevProps, nextProps) : isEqualWithFunction(prevProps, nextProps)
   );
 }
