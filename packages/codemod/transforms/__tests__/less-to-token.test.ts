936583fb17ab348da8ce3a9f58556a25c8bd7af7
dengfuping <fuping.dfp@antgroup.com>
2023-10-25 18:10:09 +0800

    chore: Format code by prettier

--- packages/codemod/transforms/__tests__/less-to-token.test.ts
+++ packages/codemod/transforms/__tests__/less-to-token.test.ts
@@ -8,9 +8,14 @@ const tests = ['antd-v4-less-to-token', 'obui-less-to-token'];
 describe(testUnit, () => {
   tests.forEach(test => {
     it(test, async () => {
-      const result = await transform(path.join(__dirname, `../__testfixtures__/less-to-token/${test}.input.less`))
-      const output = fs.readFileSync(path.join(__dirname, `../__testfixtures__/less-to-token/${test}.output.less`), 'utf-8');
+      const result = await transform(
+        path.join(__dirname, `../__testfixtures__/less-to-token/${test}.input.less`)
+      );
+      const output = fs.readFileSync(
+        path.join(__dirname, `../__testfixtures__/less-to-token/${test}.output.less`),
+        'utf-8'
+      );
       expect(result).toEqual(output);
-    })
+    });
   });
 });
