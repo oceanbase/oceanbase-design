a249190d370e87ce6bc548bdede52b4412bea4fd
dengfuping <fuping.dfp@antgroup.com>
2023-10-25 17:34:36 +0800

    feat(codemod): Add less-to-token transform

--- packages/codemod/transforms/style-to-token.js
+++ packages/codemod/transforms/style-to-token.js
@@ -1,37 +1,6 @@
-const { toLower } = require('lodash');
 const { addSubmoduleImport } = require('./utils');
+const { TOKEN_MAP, formatValue } = require('./utils/token');
 const { printOptions } = require('./utils/config');
-
-const TOKEN_MAP = {
-  // antd fixed style => antd v5 token
-  '#f0f2f5': 'colorBgLayout',
-  '#fafafa': 'colorBgLayout',
-  '#fff': 'colorBgContainer',
-  '#ffffff': 'colorBgContainer',
-  '#1890ff': 'colorInfo',
-  '#52c41a': 'colorSuccess',
-  '#73d13d': 'colorSuccess',
-  '#faad14': 'colorWarning',
-  '#ff4d4f': 'colorError',
-  '#F5222D': 'colorError',
-  '#F8636B': 'colorError',
-  '#d9d9d9': 'colorBorder',
-  '#bfbfbf': 'colorBorder',
-  'rgba(0,0,0,0.85)': 'colorText',
-  'rgba(0,0,0,0.65)': 'colorTextSecondary',
-  'rgba(0,0,0,0.45)': 'colorTextTertiary',
-  'rgba(0,0,0,0.25)': 'colorTextQuaternary',
-  'rgba(0,0,0,0.2)': 'colorFillQuaternary',
-  'rgba(0,0,0,0.04)': 'colorBgLayout',
-};
-
-function trimAll(str) {
-  return str?.replace(/(\s)*/g, '');
-}
-
-function formatValue(value) {
-  return trimAll(toLower(value));
-}
 
 function importComponent(j, root, options) {
   let hasChanged = false;
@@ -76,23 +45,6 @@ function importComponent(j, root, options) {
           });
         }
       }
-
-      // const name = path.value.declarations[0].id.name;
-      // if (/^[A-Z]/.test(name)) {
-      //   const init = path.value.declarations[0].init;
-      //   const initCode = generate(path.value).code;
-      //   if (
-      //     init &&
-      //     initCode.includes('token.') &&
-      //     // avoid duplicate statement
-      //     !initCode.includes('useToken()') &&
-      //     init.type === 'ArrowFunctionExpression'
-      //   ) {
-      //     const codeBody = init.body;
-      //     const importStyleString = `const { token } = theme.useToken();`;
-      //     codeBody.body.unshift(j.blockStatement(importStyleString).program.body[0]);
-      //   }
-      // }
     });
   }
 
