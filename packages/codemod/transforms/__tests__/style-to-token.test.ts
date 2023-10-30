a42344db52394de42893fdafc87d1a4763430390
dengfuping <fuping.dfp@antgroup.com>
2023-10-30 18:27:13 +0800

    fix(codemod): should import token for statement

--- packages/codemod/transforms/__tests__/style-to-token.test.ts
+++ packages/codemod/transforms/__tests__/style-to-token.test.ts
@@ -2,11 +2,12 @@ import { defineTest } from 'jscodeshift/src/testUtils';
 
 const testUnit = 'style-to-token';
 const tests = [
-  'function-component',
-  'class-component',
-  'static',
-  'multiple-block-statement',
-  'existed-useToken',
+  // 'function-component',
+  // 'class-component',
+  // 'block-statement',
+  // 'nested-block-statement',
+  // 'existed-useToken',
+  'statement',
 ];
 
 describe(testUnit, () => {
