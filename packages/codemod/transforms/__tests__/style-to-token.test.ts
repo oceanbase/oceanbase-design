5a6ab1b7891e92077ab53fa0eaecbdb14f8e9959
dengfuping <fuping.dfp@antgroup.com>
2023-10-31 15:20:03 +0800

    fix(codemod): should import token for non block statement including `token.`

--- packages/codemod/transforms/__tests__/style-to-token.test.ts
+++ packages/codemod/transforms/__tests__/style-to-token.test.ts
@@ -2,12 +2,12 @@ import { defineTest } from 'jscodeshift/src/testUtils';
 
 const testUnit = 'style-to-token';
 const tests = [
-  // 'function-component',
-  // 'class-component',
-  // 'block-statement',
-  // 'nested-block-statement',
-  // 'existed-useToken',
-  'statement',
+  'function-component',
+  'class-component',
+  'block-statement',
+  'nested-block-statement',
+  'existed-useToken',
+  'top-identifier',
   'case-insensitive',
 ];
 
