9e1a7139e11a72fb2c0c10d420a8b94cba6c1ec4
linhf123 <wb-lhf780012@antgroup.com>
2023-11-03 17:43:35 +0800

    feat: Add ref to Table and Add type file from antd

--- packages/design/src/table/index.tsx
+++ packages/design/src/table/index.tsx
@@ -9,6 +9,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import ConfigProvider from '../config-provider';
 import enUS from '../locale/en-US';
 import useStyle from './style';
+import { AnyObject } from '../_util/type';
 
 export * from 'antd/es/table';
 
@@ -31,7 +32,7 @@ export interface TableProps<T> extends AntTableProps<T> {
   locale?: TableLocale;
 }
 
-function Table<T>(props: TableProps<T>) {
+function Table<T>(props: TableProps<T>, ref: React.Ref<HTMLDivElement>) {
   const {
     locale: customLocale,
     columns,
@@ -63,7 +64,7 @@ function Table<T>(props: TableProps<T>) {
     className
   );
 
-  const [openPopver, setOpenPopver] = useState<boolean>(false);
+  const [openPopover, setOpenPopover] = useState<boolean>(false);
   const [currentSelectedRowKeys, setCurrentSelectedRowKeys] = useState<any[]>();
   const [currentSelectedRows, setCurrentSelectedRows] = useState<any[]>([]);
   const [currentSelectedInfo, setCurrentSelectedInfo] = useState<any>({});
@@ -118,7 +119,7 @@ function Table<T>(props: TableProps<T>) {
 
   useEffect(() => {
     if (isEmpty(currentSelectedRows) && toolSelectedContent) {
-      setOpenPopver(false);
+      setOpenPopover(false);
     }
   }, [currentSelectedRows]);
 
@@ -156,10 +157,10 @@ function Table<T>(props: TableProps<T>) {
                 overlayClassName={`${prefixCls}-batch-operation-selection-popover`}
                 content={toolSelectedContent?.(currentSelectedRowKeys, currentSelectedRows)}
                 trigger="click"
-                open={openPopver}
+                open={openPopover}
               >
-                <a onClick={() => setOpenPopver(!openPopver)}>
-                  {openPopver ? batchOperationBar?.collapse : batchOperationBar?.open}
+                <a onClick={() => setOpenPopover(!openPopover)}>
+                  {openPopover ? batchOperationBar?.collapse : batchOperationBar?.open}
                 </a>
               </Popover>
             )}
@@ -179,6 +180,7 @@ function Table<T>(props: TableProps<T>) {
   return wrapSSR(
     <AntTable
       {...props}
+      ref={ref}
       prefixCls={customizePrefixCls}
       className={tableCls}
       locale={restLocale}
@@ -213,13 +215,17 @@ function Table<T>(props: TableProps<T>) {
   );
 }
 
-Table.SELECTION_COLUMN = AntTable.SELECTION_COLUMN;
-Table.EXPAND_COLUMN = AntTable.EXPAND_COLUMN;
-Table.SELECTION_ALL = AntTable.SELECTION_ALL;
-Table.SELECTION_INVERT = AntTable.SELECTION_INVERT;
-Table.SELECTION_NONE = AntTable.SELECTION_NONE;
-Table.Column = AntTable.Column;
-Table.ColumnGroup = AntTable.ColumnGroup;
-Table.Summary = AntTable.Summary;
+const ForwardTable = React.forwardRef(Table) as <RecordType extends AnyObject = AnyObject>(
+  props: React.PropsWithChildren<TableProps<RecordType>> & { ref?: React.Ref<HTMLDivElement> }
+) => React.ReactElement;
 
-export default Table;
+export default Object.assign(ForwardTable, {
+  SELECTION_COLUMN: AntTable.SELECTION_COLUMN,
+  EXPAND_COLUMN: AntTable.EXPAND_COLUMN,
+  SELECTION_ALL: AntTable.SELECTION_ALL,
+  SELECTION_INVERT: AntTable.SELECTION_INVERT,
+  SELECTION_NONE: AntTable.SELECTION_NONE,
+  Column: AntTable.Column,
+  ColumnGroup: AntTable.ColumnGroup,
+  Summary: !AntTable.Summary,
+});
