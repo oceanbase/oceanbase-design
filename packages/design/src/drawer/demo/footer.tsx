076181dd888cb6ea589a5534ba9b81f3d8d07e60
dengfuping <fuping.dfp@antgroup.com>
2023-11-17 17:04:59 +0800

    fix(design): Drawer should add `${componentCls}-content` wrapper to overwritten antd style

--- packages/design/src/drawer/demo/footer.tsx
+++ packages/design/src/drawer/demo/footer.tsx
@@ -1,29 +1,52 @@
 import React, { useState } from 'react';
-import { Drawer, Button } from '@oceanbase/design';
+import { Drawer, Button, Space } from '@oceanbase/design';
 
 export default () => {
-  const [open, setOpen] = useState(false);
+  const [open1, setOpen1] = useState(false);
+  const [open2, setOpen2] = useState(false);
 
   return (
     <>
-      <Button
-        type="primary"
-        onClick={() => {
-          setOpen(true);
-        }}
-      >
-        Open Drawer
-      </Button>
+      <Space>
+        <Button
+          type="primary"
+          onClick={() => {
+            setOpen1(true);
+          }}
+        >
+          Enable Drawer footer by onOk
+        </Button>
+        <Button
+          onClick={() => {
+            setOpen2(true);
+          }}
+        >
+          Enable Drawer footer by footer
+        </Button>
+      </Space>
       <Drawer
-        open={open}
-        title="Title"
+        open={open1}
+        title="Enable Drawer footer by onOk"
         onOk={() => {
-          setOpen(false);
+          setOpen1(false);
         }}
         onCancel={() => {
-          setOpen(false);
+          setOpen1(false);
         }}
-        footer={true}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
+      <Drawer
+        open={open2}
+        title="Enable Drawer footer by footer"
+        onOk={() => {
+          setOpen2(false);
+        }}
+        onCancel={() => {
+          setOpen2(false);
+        }}
       >
         <p>Some contents...</p>
         <p>Some contents...</p>
--- packages/design/src/drawer/style/index.ts
+++ packages/design/src/drawer/style/index.ts
@@ -9,32 +9,38 @@ export const genDrawerStyle: GenerateStyle<DrawerToken> = (token: DrawerToken): 
 
   return {
     [`${componentCls}`]: {
-      [`${componentCls}-header`]: {
-        padding: '20px 24px 24px 24px',
-        borderBottom: 'none !important',
-      },
-      [`${componentCls}-body`]: {
-        padding: '0 24px 24px 24px',
-      },
-      [`${componentCls}-footer-content`]: {
-        position: 'absolute',
-        right: 0,
-        bottom: 0,
-        width: '100%',
-        padding: '24px 24px 20px 24px',
-        background: token.colorBgContainer,
-        zIndex: 10,
-        display: 'flex',
-        justifyContent: 'flex-end',
-        alignItems: 'center',
+      // should be wrapped by `${componentCls}-content` to overwritten antd style
+      [`${componentCls}-content`]: {
+        [`${componentCls}-header`]: {
+          padding: '20px 24px 24px 24px',
+          borderBottom: 'none !important',
+        },
+        [`${componentCls}-body`]: {
+          padding: '0 24px 24px 24px',
+        },
+        [`${componentCls}-footer-content`]: {
+          position: 'absolute',
+          right: 0,
+          bottom: 0,
+          width: '100%',
+          padding: '24px 24px 20px 24px',
+          background: token.colorBgContainer,
+          zIndex: 10,
+          display: 'flex',
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+        },
       },
     },
-    [`${componentCls}-with-footer`]: {
-      [`${componentCls}-body`]: {
-        padding: '0 24px',
-        // preserve space for footer
-        // marginBottom = paddingTop + Button height + paddingBottom
-        marginBottom: 24 + 32 + 20,
+    [`${componentCls}${componentCls}-with-footer`]: {
+      // should be wrapped by `${componentCls}-content` to overwritten antd style
+      [`${componentCls}-content`]: {
+        [`${componentCls}-body`]: {
+          padding: '0 24px',
+          // preserve space for footer
+          // marginBottom = paddingTop + Button height + paddingBottom
+          marginBottom: 24 + 32 + 20,
+        },
       },
     },
   };
