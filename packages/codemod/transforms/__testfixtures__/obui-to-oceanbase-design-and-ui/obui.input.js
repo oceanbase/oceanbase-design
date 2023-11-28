3c49b707880c8bf38acc19911c245e68233e7f2e
wt01067819 <wt01067819@antgroup.com>
2023-11-28 18:15:05 +0800

    解决 CI 报错

--- packages/codemod/transforms/__testfixtures__/obui-to-oceanbase-design-and-ui/obui.input.js
+++ packages/codemod/transforms/__testfixtures__/obui-to-oceanbase-design-and-ui/obui.input.js
@@ -1,14 +1,11 @@
-// import React from 'react';
-// import { Alert, Button, BackgroundTaskManager, BackgroundTaskManagerConstants, BasicLayout, Boundary, ConfigProvider, IconFont, Login, ModalForm, PageContainer, ProTable, Ranger, theme, TreeSearch } from '@alipay/ob-ui';
-// import type { BackgroundTaskManagerRef, ITaskMgrPreset, ITaskMgrQueue, ModalFormProps, ProTableProps, TaskMgrID } from '@alipay/ob-ui';
-// import type { BasicLayoutProps } from '@alipay/ob-ui/es/BasicLayout';
-// import type { LoginProps } from '@alipay/ob-ui/es/Login';
-// import type { PageContainerProps } from '@alipay/ob-ui/es/PageContainer';
-// import { Action } from '@alipay/ob-ui-v1';
-// import type { ActionProps } from '@alipay/ob-ui-v1/es/Action';
 import React from 'react';
-import { Alert, Button, BackgroundTaskManager, BackgroundTaskManagerConstants, BasicLayout, Boundary, ConfigProvider, IconFont, Login, ModalForm, PageContainer, ProTable, Ranger, theme, TreeSearch } from '@alipay/ob-ui';
+import { Alert, Button, BackgroundTaskManager, BackgroundTaskManagerConstants, BasicLayout, Boundary, ConfigProvider, IconFont, Login, ModalForm, ContentWithQuestion, PageContainer, ProTable, Ranger, theme, TreeSearch } from '@alipay/ob-ui';
+import type { BackgroundTaskManagerRef, ITaskMgrPreset, ITaskMgrQueue, ModalFormProps, ProTableProps, TaskMgrID } from '@alipay/ob-ui';
+import type { BasicLayoutProps } from '@alipay/ob-ui/es/BasicLayout';
+import type { LoginProps } from '@alipay/ob-ui/es/Login';
+import type { PageContainerProps } from '@alipay/ob-ui/es/PageContainer';
 import { Action } from '@alipay/ob-ui-v1';
+import type { ActionProps } from '@oceanbase/ui/es/Action';
 
 const Demo = () => {
   return <div />;
--- packages/codemod/transforms/__testfixtures__/obui-to-oceanbase-design-and-ui/obui.output.js
+++ packages/codemod/transforms/__testfixtures__/obui-to-oceanbase-design-and-ui/obui.output.js
@@ -1,16 +1,11 @@
-// import React from 'react';
-// import { Alert, Button, ConfigProvider, theme } from '@oceanbase/design';
-// import { BackgroundTaskManager, BackgroundTaskManagerConstants, BasicLayout, Boundary,  IconFont, Login, ModalForm, PageContainer, ProTable, Ranger, TreeSearch } from '@oceanbase/ui';
-// import type { BackgroundTaskManagerRef, ITaskMgrPreset, ITaskMgrQueue, ModalFormProps, ProTableProps, TaskMgrID } from '@oceanbase/ui';
-// import type { BasicLayoutProps } from '@oceanbase/ui/es/BasicLayout';
-// import type { LoginProps } from '@oceanbase/ui/es/Login';
-// import type { PageContainerProps } from '@oceanbase/ui/es/PageContainer';
-// import { Action } from '@alipay/ob-ui-v1';
-// import type { ActionProps } from '@alipay/ob-ui-v1/es/Action';
 import React from 'react';
 import { Alert, Button, ConfigProvider, theme } from '@oceanbase/design';
-import { BackgroundTaskManager, BackgroundTaskManagerConstants, BasicLayout, Boundary, IconFont, Login, ModalForm, PageContainer, ProTable, Ranger, TreeSearch } from '@oceanbase/ui';
-import { Action } from '@alipay/ob-ui-v1';
+import { BackgroundTaskManager, BackgroundTaskManagerConstants, BasicLayout, Boundary, IconFont, Login, ModalForm, PageContainer, Action, ContentWithQuestion, ProTable, Ranger, TreeSearch } from '@oceanbase/ui';
+import type { BackgroundTaskManagerRef, ITaskMgrPreset, ITaskMgrQueue, ModalFormProps, ProTableProps, TaskMgrID } from '@oceanbase/ui';
+import type { BasicLayoutProps } from '@oceanbase/ui/es/BasicLayout';
+import type { LoginProps } from '@oceanbase/ui/es/Login';
+import type { PageContainerProps } from '@oceanbase/ui/es/PageContainer';
+import type { ActionProps } from '@oceanbase/ui/es/Action';
 
 const Demo = () => {
   return <div />;
