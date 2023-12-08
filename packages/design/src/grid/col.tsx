f509c097133e1a9da7390d1136160b83e843e62a
wdy <wdy01684395@oceanbase.com>
2023-12-08 09:15:46 +0800

    fix(style): Using margin instead of gap if it does not support

--- packages/design/src/grid/col.tsx
+++ packages/design/src/grid/col.tsx
@@ -1,119 +1,21 @@
 import * as React from 'react';
-import classNames from 'classnames';
 import useFlexGapSupport from '../_util/useFlexGapSupport';
-import type { LiteralUnion } from '../_util/type';
-import { ConfigContext } from '../config-provider';
-import RowContext from 'antd/es/grid/RowContext';
-import type { RowContextState } from 'antd/es/grid/RowContext';
-import type { ColSize, ColProps } from 'antd/es/grid';
-import { useColStyle } from 'antd/es/grid/style';
+import { Col as AntCol } from 'antd';
+import type { ColProps } from 'antd/es/grid';
 
-type FlexType = number | LiteralUnion<'none' | 'auto'>;
-
-function parseFlex(flex: FlexType): string {
-  if (typeof flex === 'number') {
-    return `${flex} ${flex} auto`;
-  }
-
-  if (/^\d+(\.\d+)?(px|em|rem|%)$/.test(flex)) {
-    return `0 0 ${flex}`;
-  }
-
-  return flex;
-}
-const sizes = ['xs', 'sm', 'md', 'lg', 'xl', 'xxl'] as const;
 const Col = React.forwardRef<HTMLDivElement, ColProps>((props, ref) => {
-  const { getPrefixCls, direction } = React.useContext(ConfigContext);
-  const { gutter, wrap } = React.useContext(RowContext);
-
-  const {
-    prefixCls: customizePrefixCls,
-    span,
-    order,
-    offset,
-    push,
-    pull,
-    className,
-    children,
-    flex,
-    style,
-    ...others
-  } = props;
+  const { style, ...others } = props;
 
   const supportFlexGap = useFlexGapSupport();
-  const prefixCls = getPrefixCls('col', customizePrefixCls);
-
-  const [wrapCSSVar, hashId] = useColStyle(prefixCls);
-
-  let sizeClassObj = {};
-  sizes.forEach(size => {
-    let sizeProps: ColSize = {};
-    const propSize = props[size];
-    if (typeof propSize === 'number') {
-      sizeProps.span = propSize;
-    } else if (typeof propSize === 'object') {
-      sizeProps = propSize || {};
-    }
-
-    delete others[size];
-
-    sizeClassObj = {
-      ...sizeClassObj,
-      [`${prefixCls}-${size}-${sizeProps.span}`]: sizeProps.span !== undefined,
-      [`${prefixCls}-${size}-order-${sizeProps.order}`]: sizeProps.order || sizeProps.order === 0,
-      [`${prefixCls}-${size}-offset-${sizeProps.offset}`]:
-        sizeProps.offset || sizeProps.offset === 0,
-      [`${prefixCls}-${size}-push-${sizeProps.push}`]: sizeProps.push || sizeProps.push === 0,
-      [`${prefixCls}-${size}-pull-${sizeProps.pull}`]: sizeProps.pull || sizeProps.pull === 0,
-      [`${prefixCls}-${size}-flex-${sizeProps.flex}`]: sizeProps.flex || sizeProps.flex === 'auto',
-      [`${prefixCls}-rtl`]: direction === 'rtl',
-    };
-  });
-
-  const classes = classNames(
-    prefixCls,
-    {
-      [`${prefixCls}-${span}`]: span !== undefined,
-      [`${prefixCls}-order-${order}`]: order,
-      [`${prefixCls}-offset-${offset}`]: offset,
-      [`${prefixCls}-push-${push}`]: push,
-      [`${prefixCls}-pull-${pull}`]: pull,
-    },
-    className,
-    sizeClassObj,
-    hashId
-  );
-
-  const mergedStyle: React.CSSProperties = {};
-  // Horizontal gutter use padding
-  if (gutter && gutter[0] > 0) {
-    const horizontalGutter = gutter[0] / 2;
-    mergedStyle.paddingLeft = horizontalGutter;
-    mergedStyle.paddingRight = horizontalGutter;
+  const mergeStyle = {
+    ...style,
+  };
+  if (!supportFlexGap) {
+    mergeStyle.paddingBottom = '8px';
+    mergeStyle.paddingTop = '8px';
   }
 
-  // Vertical gutter use padding when gap not support
-  if (gutter && gutter[1] > 0 && !supportFlexGap) {
-    const verticalGutter = gutter[1] / 2;
-    mergedStyle.paddingTop = verticalGutter;
-    mergedStyle.paddingBottom = verticalGutter;
-  }
-
-  if (flex) {
-    mergedStyle.flex = parseFlex(flex);
-
-    // Hack for Firefox to avoid size issue
-    // https://github.com/ant-design/ant-design/pull/20023#issuecomment-564389553
-    if (wrap === false && !mergedStyle.minWidth) {
-      mergedStyle.minWidth = 0;
-    }
-  }
-
-  return wrapCSSVar(
-    <div {...others} style={{ ...mergedStyle, ...style }} className={classes} ref={ref}>
-      {children}
-    </div>
-  );
+  return <AntCol ref={ref} style={mergeStyle} {...others} />;
 });
 
 if (process.env.NODE_ENV !== 'production') {
--- packages/design/src/space/index.md
+++ packages/design/src/space/index.md
@@ -10,6 +10,6 @@ nav:
 
 ## 代码演示
 
-<code src="./demo/basic.tsx" title="基本用法"></code> <code src="./demo/size.tsx" title="间距大小"></code> <code src="./demo/vertical.tsx" title="相邻组件垂直间距"></code> <code src="./demo/wrap.tsx" title="自动换行"></code>
+<code src="./demo/basic.tsx" title="基本用法"></code> </code> <code src="./demo/vertical.tsx" title="相邻组件垂直间距"></code> <code src="./demo/wrap.tsx" title="自动换行"></code>
 
 - 详见 antd Space 文档: https://ant.design/components/space-cn/
