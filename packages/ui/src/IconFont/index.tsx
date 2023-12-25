e90eaf91a28c6eabf7d34ffcd75c260e9084ec36
dengfuping <fuping.dfp@antgroup.com>
2023-12-25 18:23:22 +0800

    fix(ui): Remove IconFont built-in script url and mark IconFont as @deprecated

--- packages/ui/src/IconFont/index.tsx
+++ packages/ui/src/IconFont/index.tsx
@@ -1,4 +1,3 @@
-import { createFromIconfontCN } from '@oceanbase/icons';
 import React from 'react';
 import './font/iconfont.css';
 
@@ -8,55 +7,12 @@ export interface IconFontProps {
   style?: React.CSSProperties;
 }
 
-const CustomIcon = createFromIconfontCN({
-  // 在 iconfont.cn 上生成
-  scriptUrl: '//at.alicdn.com/t/a/font_3786261_ifhixq9j5c.js',
-});
-
-const iconfontList = [
-  'backup',
-  'backup-colored',
-  'cluster',
-  'cluster-colored',
-  'data-source',
-  'data-source-colored',
-  'docs',
-  'diagnosis',
-  'diagnosis-colored',
-  'host',
-  'host-colored',
-  'log',
-  'log-colored',
-  'migration',
-  'migration-colored',
-  'monitor',
-  'monitor-colored',
-  'notification',
-  'obproxy',
-  'obproxy-colored',
-  'package',
-  'package-colored',
-  'overview',
-  'overview-colored',
-  'property',
-  'property-colored',
-  'tenant',
-  'tenant-colored',
-  'sync',
-  'sync-colored',
-  'system',
-  'system-colored',
-  'user',
-];
-
+/**
+ * @deprecated Please use `@oceanbase/icons` instead.
+ */
 const IconFont = (props: IconFontProps) => {
   const { type, className, ...restProps } = props;
-  // TODO: 后续建议统一使用 iconfont.cn 上的图标，这里先兼容两种用法
-  return iconfontList.includes(type) ? (
-    <CustomIcon type={type} className={className} {...restProps} />
-  ) : (
-    <i className={`iconfont ${type} ${className}`} {...restProps} />
-  );
+  return <i className={`iconfont ${type} ${className}`} {...restProps} />;
 };
 
 export default IconFont;
