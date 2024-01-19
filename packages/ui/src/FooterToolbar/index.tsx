aab64785e024feae0d89d80e246b418fde950ec5
dengfuping <fuping.dfp@antgroup.com>
2024-01-19 17:25:05 +0800

    improve(ui): FooterToolbar children use large size and middle font-size

--- packages/ui/src/FooterToolbar/index.tsx
+++ packages/ui/src/FooterToolbar/index.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { FooterToolbar as AntFooterToolbar } from '@ant-design/pro-components';
 import type { FooterToolbarProps as AntFooterToolbarProps } from '@ant-design/pro-layout/es/components/FooterToolbar';
-import { ConfigProvider } from '@oceanbase/design';
+import { ConfigProvider, theme } from '@oceanbase/design';
 import useStyle from './style';
 
 export type FooterToolbarProps = AntFooterToolbarProps;
@@ -10,14 +10,30 @@ const FooterToolbar: React.FC<FooterToolbarProps> = ({
   // render footer under parent instead of body by default
   portalDom = false,
   prefixCls: customizePrefixCls,
+  children,
   ...restProps
 }) => {
   const { getPrefixCls } = useContext(ConfigProvider.ConfigContext);
   const prefixCls = getPrefixCls('pro-footer-bar', customizePrefixCls);
   const { wrapSSR } = useStyle(prefixCls);
 
+  const { token } = theme.useToken();
+
   return wrapSSR(
-    <AntFooterToolbar portalDom={portalDom} prefixCls={customizePrefixCls} {...restProps} />
+    <AntFooterToolbar portalDom={portalDom} prefixCls={customizePrefixCls} {...restProps}>
+      <ConfigProvider
+        // large size component
+        componentSize="large"
+        // middle font size
+        theme={{
+          token: {
+            fontSizeLG: token.fontSize,
+          },
+        }}
+      >
+        {children}
+      </ConfigProvider>
+    </AntFooterToolbar>
   );
 };
 
