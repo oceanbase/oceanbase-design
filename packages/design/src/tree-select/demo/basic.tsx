623483ef362a361dd2408bc2a2324cb02eb92c23
dengfuping <fuping.dfp@antgroup.com>
2024-04-07 14:43:00 +0800

    feat(design): TreeSelect add default placeholder and ConfigProvider support to config it global

---
+++ packages/design/src/tree-select/demo/basic.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { TreeSelect } from '@oceanbase/design';
+
+const App: React.FC = () => {
+  const [value, setValue] = useState<string>();
+
+  const onChange = (newValue: string) => {
+    setValue(newValue);
+  };
+
+  const treeData = [
+    {
+      value: 'parent 1',
+      title: 'parent 1',
+      children: [
+        {
+          value: 'parent 1-0',
+          title: 'parent 1-0',
+          children: [
+            {
+              value: 'leaf1',
+              title: 'leaf1',
+            },
+            {
+              value: 'leaf2',
+              title: 'leaf2',
+            },
+          ],
+        },
+        {
+          value: 'parent 1-1',
+          title: 'parent 1-1',
+          children: [
+            {
+              value: 'leaf3',
+              title: <b style={{ color: '#006AFF' }}>leaf3</b>,
+            },
+          ],
+        },
+      ],
+    },
+  ];
+
+  return (
+    <TreeSelect
+      showSearch
+      allowClear
+      treeDefaultExpandAll
+      treeData={treeData}
+      value={value}
+      onChange={onChange}
+      style={{ width: '100%' }}
+    />
+  );
+};
+
+export default App;
