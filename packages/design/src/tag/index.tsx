123f788734a04eacb012b43854a151b272fb81fd
linhf123 <wb-lhf780012@antgroup.com>
2024-09-04 10:49:40 +0800

    chore: Add comment

--- packages/design/src/tag/index.tsx
+++ packages/design/src/tag/index.tsx
@@ -54,6 +54,7 @@ const Tag = React.forwardRef<HTMLSpanElement, TagProps>(
       >
         {ellipsisConfig ? (
           <Typography.Text ellipsis={ellipsisConfig}>
+            {/* Typography.Text 存在 ellipsis 配置时 ，将 icon 放在 Typography.Text 内部，避免溢出时与 icon 发生样式冲突。这里保留 Typography.Text 主要为了使用 Typography.Text 的判断内容溢出展示 Tooltip 的能力，自定义实现成本过大 */}
             {realIcon}
             {children}
           </Typography.Text>
