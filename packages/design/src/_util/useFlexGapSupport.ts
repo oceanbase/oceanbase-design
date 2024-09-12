2843f14eeee23eef04c84e8c45106cfab6eaed70
dengfuping <fuping.dfp@antgroup.com>
2024-09-12 16:29:55 +0800

    fix(design): Space should be flexible by default to avoid shrink

--- packages/design/src/_util/useFlexGapSupport.ts
+++ packages/design/src/_util/useFlexGapSupport.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { detectFlexGapSupported } from './styleChecker';
 
 export default () => {
-  const [flexible, setFlexible] = React.useState(false);
+  const [flexible, setFlexible] = React.useState(true);
   React.useEffect(() => {
     setFlexible(detectFlexGapSupported());
   }, []);
