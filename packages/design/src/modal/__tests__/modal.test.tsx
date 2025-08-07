fd2dc1c42718e34b0b7183479f1da859bb93113d
诸岳 <fuping.dfp@antgroup.com>
2025-08-07 14:03:55 +0800

    feat(design): Modal add extra prop (#1130)

--- packages/design/src/modal/__tests__/modal.test.tsx
+++ packages/design/src/modal/__tests__/modal.test.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
 import { render } from '@testing-library/react';
 import { Modal } from '@oceanbase/design';
-import type { ModalProgressProps } from '@oceanbase/design/es/modal';
+import type { ModalProps } from '@oceanbase/design/es/modal';
 
-const ModalTest: React.FC<ModalProgressProps> = props => {
+const ModalTest: React.FC<ModalProps> = props => {
   const [open, setOpen] = React.useState(false);
   const container = React.useRef<HTMLDivElement>(null);
   useEffect(() => {
@@ -33,6 +33,13 @@ describe('Modal', () => {
   it('should hide footer dom when footer is false', () => {
     const { container, asFragment } = render(<ModalTest footer={false} />);
     expect(container.querySelector('.ant-modal-footer')).toBeFalsy();
+    expect(asFragment().firstChild).toMatchSnapshot();
+  });
+
+  it('extra prop should work', () => {
+    const { container, asFragment } = render(<ModalTest extra={<a>extra</a>} />);
+    expect(container.querySelector('.ant-modal-footer-content')).toBeTruthy();
+    expect(container.querySelector('.ant-modal-footer-extra')).toBeTruthy();
     expect(asFragment().firstChild).toMatchSnapshot();
   });
 });
