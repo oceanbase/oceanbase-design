813d9cc1201ae20232198f60f99b4375f725f3e4
Richard Zhang <aaaayu1019@gmail.com>
2025-08-29 16:59:11 +0800

    feat(ui): add Filter component

---
+++ packages/ui/src/Filter/components/FilterSelect.tsx
@@ -0,0 +1,97 @@
+import { Button, Flex, Popover, Space } from '@oceanbase/design';
+import { DownOutlined } from '@oceanbase/icons';
+import type { FC } from 'react';
+import { useMemo, useState } from 'react';
+import ClearCircle from './ClearCircle';
+import FilterSearch from './FilterSearch';
+
+import { getPrefix } from '../../_util';
+import '../styles.less';
+
+export interface PageExtraOperatorProps {
+  label: string;
+  value: string;
+  setValue: (value: string) => void;
+  options: {
+    label: string;
+    value: string;
+  }[];
+  clear: () => void;
+  showSearch?: boolean;
+}
+
+/**
+ * 事件操作者选择器
+ * @param param0 组件props
+ * @returns
+ */
+const PageExtraOperator: FC<PageExtraOperatorProps> = ({
+  label,
+  value,
+  setValue,
+  clear,
+  options,
+  showSearch = false,
+}) => {
+  const [searchOperator, setSearchOperator] = useState<string>('');
+  const prefix = getPrefix('filter-select');
+
+  // 根据搜索条件过滤数据
+  const filterOptions = useMemo(() => {
+    if (!searchOperator) {
+      return options;
+    }
+    return options?.filter(item => item.label.toLowerCase().includes(searchOperator.toLowerCase()));
+  }, [options, searchOperator]);
+
+  return (
+    <Popover
+      key="operator"
+      placement="bottomLeft"
+      arrow={false}
+      trigger="click"
+      content={
+        <div>
+          {showSearch && <FilterSearch setValue={setSearchOperator} />}
+          <Flex
+            vertical
+            style={{ marginTop: showSearch ? 8 : 0, maxHeight: 244, overflow: 'scroll' }}
+          >
+            {filterOptions?.map(item => (
+              <div
+                className={`${prefix}-item`}
+                key={item.value}
+                onClick={() => {
+                  setValue(item.value);
+                }}
+              >
+                {item.label}
+              </div>
+            ))}
+          </Flex>
+        </div>
+      }
+    >
+      <Button>
+        {value ? (
+          <Space>
+            {value}
+            <ClearCircle
+              clear={() => {
+                clear();
+                setSearchOperator('');
+              }}
+            />
+          </Space>
+        ) : (
+          <Space>
+            {label}
+            <DownOutlined />
+          </Space>
+        )}
+      </Button>
+    </Popover>
+  );
+};
+
+export default PageExtraOperator;
---
+++ packages/ui/src/Filter/index.md
@@ -0,0 +1,31 @@
+---
+title: Filter 筛选处理
+nav:
+  title: 业务组件
+  path: /biz-components
+---
+
+## 代码演示
+
+<code src="./demo/basic.tsx" title="基本使用" description="一般用于Table组件的Extra作为筛选组件处理"></code> <code src="./demo/search.tsx" title="开启搜索" description="在筛选面板中对选择项进行搜索"></code>
+
+## Filter.Cascader API
+
+| 参数           | 说明                   | 类型       | 默认值 | 版本 |     |
+| :------------- | :--------------------- | :--------- | :----- | :--- | --- |
+| label          | 默认展示的文字内容     | String     | -      | -    |
+| haveValueLabel | 有值后展示的文字内容   | String     | -      | -    |
+| clear          | 点击清除按钮的回调函数 | () => void | -      | -    |
+| showSearch     | 是否开启搜索           | boolean    | false  | -    |
+
+- 详见 antd Cascader 文档: https://ant.design/components/cascader-cn/
+
+## Filter.Select API
+
+| 参数       | 说明                   | 类型          | 默认值 | 版本 |     |
+| :--------- | :--------------------- | :------------ | :----- | :--- | --- |
+| label      | 默认展示的文字内容     | String        | -      | -    |
+| value      | 选择的值               | String        | -      | -    |
+| setValue   | 设置选择的值           | (val) => void | -      | -    |
+| clear      | 点击清除按钮的回调函数 | () => void    | -      | -    |
+| showSearch | 是否开启搜索           | boolean       | false  | -    |
