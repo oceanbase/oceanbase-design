5a4db69aad24934d1a0ad1ceb039d6816fd2a3c2
dengfuping <dengfuping@users.noreply.github.com>
2025-08-29 07:23:26 +0000

    deploy: 7d514b33aa41276c83c58b0e8f2bc9e2ad8d08ff

---
+++ docs__design__design-use-with-nextjs.md.b1c58107.async.js
@@ -0,0 +1,16 @@
+"use strict";(self.webpackChunkroot=self.webpackChunkroot||[]).push([[2746],{28177:function(d,t,_){_.r(t);var s=_(92759),u=_(40671),m=_(46072),c=_(30016),p=_(93366),a=_(91e3),g=_(73953),E=_(58493),h=_(96125),b=_(8544),v=_(32778),D=_(1768),P=_(89457),x=_(36899),M=_(57707),O=_(15069),I=_(50936),f=_(77562),C=_(49161),i=_(13198),r=_(14154),o=_(75271),n=_(56893),e=_(20769);function l(){return(0,e.tZ)(i.dY,null,(0,e.tZ)(o.Suspense,{fallback:(0,e.tZ)(r.Z,null)},(0,e.tZ)(o.Fragment,null,(0,e.tZ)("div",{className:"markdown"},(0,e.tZ)("ul",null,(0,e.tZ)("li",null,n.texts[0].value,(0,e.tZ)("code",null,n.texts[1].value),n.texts[2].value,(0,e.tZ)("code",null,n.texts[3].value),n.texts[4].value)),(0,e.tZ)(a.Z,{lang:"bash"},n.texts[5].value),(0,e.tZ)(a.Z,{lang:"ts"},n.texts[6].value),(0,e.tZ)("ul",null,(0,e.tZ)("li",null,n.texts[7].value,(0,e.tZ)("a",{href:"https://stackblitz.com/edit/nextjs-oceanbase-design",target:"_blank"},n.texts[8].value),n.texts[9].value)),(0,e.tZ)("iframe",{src:"https://stackblitz.com/edit/nextjs-oceanbase-design?embed=1&file=app%2Fpage.tsx",style:{width:"100%",height:"500px",border:"0",borderRadius:"4px",overflow:"hidden"},title:"@oceanbase/ui reproduction template",allow:"accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking",sandbox:"allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"})))))}t.default=l},56893:function(d,t,_){_.r(t),_.d(t,{texts:function(){return s}});const s=[{value:"\u4E3A\u4E86\u548C ",paraId:0},{value:"@oceanbase/design",paraId:0},{value:" \u642D\u914D\u4F7F\u7528\uFF0C\u9700\u8981\u5B89\u88C5 ",paraId:0},{value:"file-loader",paraId:0},{value:" \u4F9D\u8D56\uFF0C\u5E76\u4FEE\u6539 Next.js \u914D\u7F6E\uFF1A",paraId:0},{value:`npm install file-loader --save-dev
+`,paraId:1},{value:`const nextConfig = {
+  // to transpile some special packages to es5
+  transpilePackages: ['@oceanbase', 'query-string', '@ant-design', '@ant-design/cssinjs'],
+  webpack: config => {
+    // to handle @oceanbase/design built-in font files
+    config.module.rules.push({
+      test: /\\.(woff|woff2|ttf?g)$/i,
+      use: ['file-loader'],
+    });
+    return config;
+  },
+};
+
+export default nextConfig;
+`,paraId:2},{value:"\u5B8C\u6574\u914D\u7F6E\u548C\u53EF\u8FD0\u884C\u7684\u793A\u4F8B\uFF0C\u53EF\u4EE5\u53C2\u8003\u6211\u4EEC\u63D0\u4F9B\u7684 ",paraId:3},{value:"Next.js + @oceanbase/design \u9879\u76EE\u6A21\u677F",paraId:3},{value:"\u3002",paraId:3}]}}]);
---
+++ docs__ui__ui-quickstart.md.bb724215.async.js
@@ -0,0 +1,14 @@
+"use strict";(self.webpackChunkroot=self.webpackChunkroot||[]).push([[6824],{56645:function(d,n,_){_.r(n);var a=_(92759),m=_(40671),l=_(46072),c=_(30016),E=_(93366),o=_(91e3),p=_(73953),h=_(58493),b=_(96125),g=_(8544),D=_(32778),x=_(1768),P=_(89457),M=_(36899),O=_(57707),v=_(15069),I=_(50936),C=_(77562),B=_(49161),i=_(13198),r=_(14154),s=_(75271),t=_(29504),e=_(20769);function u(){return(0,e.tZ)(i.dY,null,(0,e.tZ)(s.Suspense,{fallback:(0,e.tZ)(r.Z,null)},(0,e.tZ)(s.Fragment,null,(0,e.tZ)("div",{className:"markdown"},(0,e.tZ)("h2",{id:"\u4F7F\u7528-codesandbox-\u5FEB\u901F\u521B\u5EFA"},(0,e.tZ)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u4F7F\u7528-codesandbox-\u5FEB\u901F\u521B\u5EFA"},(0,e.tZ)("span",{className:"icon icon-link"})),"\u4F7F\u7528 CodeSandbox \u5FEB\u901F\u521B\u5EFA"),(0,e.tZ)("ul",null,(0,e.tZ)("li",null,t.texts[0].value,(0,e.tZ)("a",{href:"https://codesandbox.io/s/oceanbase-ui-reproduction-template-cknd9y",target:"_blank"},t.texts[1].value),t.texts[2].value)),(0,e.tZ)("iframe",{src:"https://codesandbox.io/embed/oceanbase-ui-reproduction-template-cknd9y?fontsize=14&hidenavigation=1&theme=dark",style:{width:"100%",height:"500px",border:"0",borderRadius:"4px",overflow:"hidden"},title:"@oceanbase/ui reproduction template",allow:"accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking",sandbox:"allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"}),(0,e.tZ)("h2",{id:"\u5728\u9879\u76EE\u4E2D\u4F7F\u7528"},(0,e.tZ)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u5728\u9879\u76EE\u4E2D\u4F7F\u7528"},(0,e.tZ)("span",{className:"icon icon-link"})),"\u5728\u9879\u76EE\u4E2D\u4F7F\u7528"),(0,e.tZ)("h3",{id:"\u5B89\u88C5"},(0,e.tZ)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u5B89\u88C5"},(0,e.tZ)("span",{className:"icon icon-link"})),"\u5B89\u88C5"),(0,e.tZ)(o.Z,{lang:"bash"},t.texts[3].value),(0,e.tZ)("h3",{id:"\u793A\u4F8B"},(0,e.tZ)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u793A\u4F8B"},(0,e.tZ)("span",{className:"icon icon-link"})),"\u793A\u4F8B"),(0,e.tZ)(o.Z,{lang:"jsx"},t.texts[4].value)))))}n.default=u},29504:function(d,n,_){_.r(n),_.d(n,{texts:function(){return a}});const a=[{value:"\u53EF\u4EE5\u4F7F\u7528\u6211\u4EEC\u63D0\u4F9B\u7684 ",paraId:0,tocIndex:0},{value:"CodeSandbox \u6A21\u677F",paraId:0,tocIndex:0},{value:" \u5FEB\u901F\u521B\u5EFA\u548C\u9884\u89C8\u3002",paraId:0,tocIndex:0},{value:`$ npm i @oceanbase/ui --save
+`,paraId:1,tocIndex:2},{value:`import { Lottie } from '@oceanbase/ui';
+
+const App = () => {
+  return (
+    <Lottie
+      path="https://assets9.lottiefiles.com/packages/lf20_WPqksadnNs.json"
+      style={{
+        height: 200,
+      }}
+    />
+  );
+};
+`,paraId:2,tocIndex:3}]}}]);
