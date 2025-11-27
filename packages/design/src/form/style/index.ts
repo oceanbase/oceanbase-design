f1b790100ad7a357a9af7741a1c728b29d18dbc7
dengfuping <fuping.dfp@antgroup.com>
2025-11-27 12:31:56 +0800

    feat(design): Form.Item add description prop and adjust extra style

--- packages/design/src/form/style/index.ts
+++ packages/design/src/form/style/index.ts
@@ -7,6 +7,12 @@ export type FormToken = FullToken<'Form'>;
 export const genFormStyle: GenerateStyle<FormToken> = (token: FormToken): CSSObject => {
   const { componentCls } = token;
   return {
+    [componentCls]: {
+      [`${componentCls}-item-extra`]: {
+        paddingTop: token.paddingXXS,
+        fontSize: token.fontSizeSM,
+      },
+    },
     [`${componentCls}${componentCls}-vertical`]: {
       [`${componentCls}-item:not(${componentCls}-item-horizontal)`]: {
         [`${componentCls}-item-label > label`]: {
@@ -15,6 +21,19 @@ export const genFormStyle: GenerateStyle<FormToken> = (token: FormToken): CSSObj
             position: 'absolute',
             right: 0,
           },
+        },
+      },
+      [`${componentCls}-item-has-description`]: {
+        [`${componentCls}-item-control-input`]: {
+          minHeight: 'auto',
+        },
+        [`${componentCls}-item-label`]: {
+          paddingBottom: token.paddingXXS,
+        },
+        [`${componentCls}-item-description`]: {
+          paddingBottom: token.paddingXXS + 2,
+          fontSize: token.fontSizeSM,
+          color: token.colorTextDescription,
         },
       },
     },
