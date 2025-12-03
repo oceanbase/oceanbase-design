65fcd2b369fe69cdac46552a35e261332ecb2fa3
诸岳 <fuping.dfp@antgroup.com>
2025-12-03 18:33:53 +0800

    improve(design): InputNumber addon font-size and color (#1295)

---
+++ packages/design/src/input-number/style/index.ts
@@ -0,0 +1,30 @@
+import type { CSSObject } from '@ant-design/cssinjs';
+import type { FullToken, GenerateStyle } from '../../theme/interface';
+import { genComponentStyleHook } from '../../_util/genComponentStyleHook';
+
+export type InputNumberToken = FullToken<'InputNumber'>;
+
+export const genInputStyle: GenerateStyle<InputNumberToken> = (
+  token: InputNumberToken
+): CSSObject => {
+  const { antCls, componentCls } = token;
+  const selectComponentCls = `${antCls}-select-selector`;
+  return {
+    [`${componentCls}-group-wrapper`]: {
+      [`${componentCls}-group-addon`]: {
+        color: token.colorTextTertiary,
+        fontSize: token.fontSizeSM,
+        [selectComponentCls]: {
+          color: token.colorTextTertiary,
+        },
+      },
+    },
+  };
+};
+
+export default (prefixCls: string) => {
+  const useStyle = genComponentStyleHook('InputNumber', token => {
+    return [genInputStyle(token as InputNumberToken)];
+  });
+  return useStyle(prefixCls);
+};
