99fb5cdc834ad1ae564dcd16fb93b4f8e426d798
dengfuping <dengfuping@users.noreply.github.com>
2025-12-04 07:06:52 +0000

    deploy: ca53c6686a0e6b1b16c84672d8bdd15a720d9a03

---
+++ docs__charts__charts-quickstart.md.acb15752.async.js
@@ -0,0 +1,37 @@
+"use strict";(self.webpackChunkroot=self.webpackChunkroot||[]).push([[8640],{22738:function(d,n,_){_.r(n);var t=_(18534),m=_(35077),l=_(77924),c=_(23130),E=_(70001),o=_(52036),h=_(59675),p=_(81001),b=_(95047),g=_(71354),x=_(69015),D=_(54095),P=_(35355),v=_(75448),M=_(46865),O=_(72435),I=_(47339),C=_(90539),y=_(71815),i=_(11226),r=_(4299),s=_(75271),a=_(44058),e=_(56517);function u(){return(0,e.tZ)(i.dY,null,(0,e.tZ)(s.Suspense,{fallback:(0,e.tZ)(r.Z,null)},(0,e.tZ)(s.Fragment,null,(0,e.tZ)("div",{className:"markdown"},(0,e.tZ)("h2",{id:"\u4F7F\u7528-codesandbox-\u5FEB\u901F\u521B\u5EFA"},(0,e.tZ)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u4F7F\u7528-codesandbox-\u5FEB\u901F\u521B\u5EFA"},(0,e.tZ)("span",{className:"icon icon-link"})),"\u4F7F\u7528 CodeSandbox \u5FEB\u901F\u521B\u5EFA"),(0,e.tZ)("ul",null,(0,e.tZ)("li",null,a.texts[0].value,(0,e.tZ)("a",{href:"https://codesandbox.io/s/oceanbase-charts-reproduction-template-dlkw72",target:"_blank"},a.texts[1].value),a.texts[2].value)),(0,e.tZ)("iframe",{src:"https://codesandbox.io/embed/oceanbase-charts-reproduction-template-dlkw72?fontsize=14&hidenavigation=1&theme=dark",style:{width:"100%",height:"500px",border:"0",borderRadius:"4px",overflow:"hidden"},title:"@oceanbase/charts reproduction template",allow:"accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking",sandbox:"allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"}),(0,e.tZ)("h2",{id:"\u5728\u9879\u76EE\u4E2D\u4F7F\u7528"},(0,e.tZ)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u5728\u9879\u76EE\u4E2D\u4F7F\u7528"},(0,e.tZ)("span",{className:"icon icon-link"})),"\u5728\u9879\u76EE\u4E2D\u4F7F\u7528"),(0,e.tZ)("h3",{id:"\u5B89\u88C5"},(0,e.tZ)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u5B89\u88C5"},(0,e.tZ)("span",{className:"icon icon-link"})),"\u5B89\u88C5"),(0,e.tZ)(o.Z,{lang:"bash"},a.texts[3].value),(0,e.tZ)("h3",{id:"\u793A\u4F8B"},(0,e.tZ)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u793A\u4F8B"},(0,e.tZ)("span",{className:"icon icon-link"})),"\u793A\u4F8B"),(0,e.tZ)(o.Z,{lang:"jsx"},a.texts[4].value)))))}n.default=u},44058:function(d,n,_){_.r(n),_.d(n,{texts:function(){return t}});const t=[{value:"\u53EF\u4EE5\u4F7F\u7528\u6211\u4EEC\u63D0\u4F9B\u7684 ",paraId:0,tocIndex:0},{value:"CodeSandbox \u6A21\u677F",paraId:0,tocIndex:0},{value:" \u5FEB\u901F\u521B\u5EFA\u548C\u9884\u89C8\u56FE\u8868\u3002",paraId:0,tocIndex:0},{value:`$ npm i @oceanbase/charts --save
+`,paraId:1,tocIndex:2},{value:`import { Bar } from '@oceanbase/charts';
+
+export default () => {
+  const data = [
+    {
+      year: '1951 \u5E74',
+      value: 38,
+    },
+    {
+      year: '1952 \u5E74',
+      value: 52,
+    },
+    {
+      year: '1956 \u5E74',
+      value: 61,
+    },
+    {
+      year: '1957 \u5E74',
+      value: 138,
+    },
+    {
+      year: '1958 \u5E74',
+      value: 48,
+    },
+  ];
+  const config = {
+    data,
+    xField: 'value',
+    yField: 'year',
+    legend: {
+      position: 'top-left',
+    },
+  };
+  return <Bar {...config} />;
+};
+`,paraId:2,tocIndex:3}]}}]);
