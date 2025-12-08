45c839be0fe32ba9c378e54d3bd37110111cfc7e
诸岳 <fuping.dfp@antgroup.com>
2025-12-08 14:28:00 +0800

    fix(design): Form.Item renderProps should work (#1310)

--- packages/design/src/form/style/index.ts
+++ packages/design/src/form/style/index.ts
@@ -8,6 +8,7 @@ export const genFormStyle: GenerateStyle<FormToken> = (token: FormToken): CSSObj
   const { componentCls, calc } = token;
   return {
     [componentCls]: {
+      // extra style
       [`${componentCls}-item-explain, ${componentCls}-item-extra`]: {
         paddingTop: token.paddingXXS,
         fontSize: token.fontSizeSM,
@@ -16,27 +17,28 @@ export const genFormStyle: GenerateStyle<FormToken> = (token: FormToken): CSSObj
         paddingTop: 0,
       },
     },
+    // vertical layout style
     [`${componentCls}${componentCls}-vertical`]: {
       [`${componentCls}-item:not(${componentCls}-item-horizontal)`]: {
+        // description style
+        [`${componentCls}-item-control-input`]: {
+          minHeight: 'auto',
+        },
+        [`${componentCls}-item-label`]: {
+          paddingBottom: calc(token.paddingXXS).add(2).equal(),
+        },
+        [`${componentCls}-item-description`]: {
+          paddingTop: token.paddingXXS,
+          fontSize: token.fontSizeSM,
+          color: token.colorTextDescription,
+        },
+        // action style
         [`${componentCls}-item-label > label`]: {
           width: '100%',
           [`${componentCls}-item-action`]: {
             position: 'absolute',
             right: 0,
           },
-        },
-      },
-      [`${componentCls}-item-has-description`]: {
-        [`${componentCls}-item-control-input`]: {
-          minHeight: 'auto',
-        },
-        [`${componentCls}-item-label`]: {
-          paddingBottom: token.paddingXXS,
-        },
-        [`${componentCls}-item-description`]: {
-          paddingBottom: calc(token.paddingXXS).add(2).equal(),
-          fontSize: token.fontSizeSM,
-          color: token.colorTextDescription,
         },
       },
     },
