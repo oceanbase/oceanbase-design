1694e6ba4b04ad05d3feecebca7e78a6448e4c1d
dengfuping <fuping.dfp@antgroup.com>
2025-12-10 10:31:15 +0800

    feat(codemod): style-to-token and less-to-token support to rewrite fontWeight and borderRadius token

--- packages/codemod/transforms/__testfixtures__/style-to-token/function-component.output.js
+++ packages/codemod/transforms/__testfixtures__/style-to-token/function-component.output.js
@@ -6,8 +6,8 @@ function Demo1() {
   const tokenList = [token.colorTextTertiary, token.colorInfo, token.colorBgLayout];
   return (
     <div>
-      <Alert style={{ color: token.colorText, background: token.colorTextSecondary, backgroundColor: token.colorTextTertiary, border: `1px solid ${token.colorBorder}`, fontSize: token.fontSize }} />
-      <Button style={{ color: token.colorInfo, background: token.colorSuccess, backgroundColor: token.colorWarning, borderColor: token.colorError, fontSize: token.fontSizeSM }}></Button>
+      <Alert style={{ color: token.colorText, background: token.colorTextSecondary, backgroundColor: token.colorTextTertiary, border: `1px solid ${token.colorBorder}`, fontSize: token.fontSize, fontWeight: token.fontWeight, borderRadius: token.borderRadius }} />
+      <Button style={{ color: token.colorInfo, background: token.colorSuccess, backgroundColor: token.colorWarning, borderColor: token.colorError, fontSize: token.fontSizeSM, fontWeight: token.fontWeightStrong, borderRadius: token.borderRadiusMD }}></Button>
       <Tooltip color={token.colorBgContainer} backgroundColor={token.colorErrorBg} borderColor={token.colorBgLayout} border={`1px solid ${token.colorBgLayout}`} />
     </div>
   );
