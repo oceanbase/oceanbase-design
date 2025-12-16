dc80e90faf9890441ef45da1b72cbe7c2b58c8f5
dengfuping <fuping.dfp@antgroup.com>
2025-12-16 20:25:49 +0800

    improve(design): notification colorBg, borderRadius and message borderRadius

--- packages/design/src/notification/demo/basic.tsx
+++ packages/design/src/notification/demo/basic.tsx
@@ -1,56 +1,53 @@
 import React from 'react';
-import { Button, ConfigProvider, notification, Space, theme } from '@oceanbase/design';
+import { Button, Space, notification } from '@oceanbase/design';
 
 export default () => {
-  // 在应用外围包裹一次 ConfigProvider 即可
   return (
-    <ConfigProvider>
-      <Space>
-        <Button
-          onClick={() => {
-            notification.info({
-              message: 'Notification Title',
-              description:
-                'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
-            });
-          }}
-        >
-          Info
-        </Button>
-        <Button
-          onClick={() => {
-            notification.success({
-              message: 'Notification Title',
-              description:
-                'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
-            });
-          }}
-        >
-          Success
-        </Button>
-        <Button
-          onClick={() => {
-            notification.error({
-              message: 'Notification Title',
-              description:
-                'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
-            });
-          }}
-        >
-          Error
-        </Button>
-        <Button
-          onClick={() => {
-            notification.warning({
-              message: 'Notification Title',
-              description:
-                'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
-            });
-          }}
-        >
-          Warning
-        </Button>
-      </Space>
-    </ConfigProvider>
+    <Space>
+      <Button
+        onClick={() => {
+          notification.info({
+            message: 'Notification Title',
+            description:
+              'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+          });
+        }}
+      >
+        Info
+      </Button>
+      <Button
+        onClick={() => {
+          notification.success({
+            message: 'Notification Title',
+            description:
+              'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+          });
+        }}
+      >
+        Success
+      </Button>
+      <Button
+        onClick={() => {
+          notification.error({
+            message: 'Notification Title',
+            description:
+              'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+          });
+        }}
+      >
+        Error
+      </Button>
+      <Button
+        onClick={() => {
+          notification.warning({
+            message: 'Notification Title',
+            description:
+              'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+          });
+        }}
+      >
+        Warning
+      </Button>
+    </Space>
   );
 };
