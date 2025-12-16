bf21e8c4c46ca999f1fd97eea0513b4ebc35036b
诸岳 <fuping.dfp@antgroup.com>
2025-12-16 20:30:52 +0800

    improve(design): notification colorBg, borderRadius and message borderRadius (#1344)

--- packages/design/src/message/demo/basic.tsx
+++ packages/design/src/message/demo/basic.tsx
@@ -1,46 +1,43 @@
-import { Button, ConfigProvider, message, Space } from '@oceanbase/design';
+import { Button, Space, message } from '@oceanbase/design';
 
 export default () => {
-  // 在应用外围包裹一次 ConfigProvider 即可
   return (
-    <ConfigProvider>
-      <Space>
-        <Button
-          onClick={() => {
-            message.info('This is a info message');
-          }}
-        >
-          Info
-        </Button>
-        <Button
-          onClick={() => {
-            message.success('This is a success message');
-          }}
-        >
-          Success
-        </Button>
-        <Button
-          onClick={() => {
-            message.error('This is an error message');
-          }}
-        >
-          Error
-        </Button>
-        <Button
-          onClick={() => {
-            message.warning('This is a warning message');
-          }}
-        >
-          Warning
-        </Button>
-        <Button
-          onClick={() => {
-            message.loading('This is a loading message');
-          }}
-        >
-          Loading
-        </Button>
-      </Space>
-    </ConfigProvider>
+    <Space>
+      <Button
+        onClick={() => {
+          message.info('This is a info message');
+        }}
+      >
+        Info
+      </Button>
+      <Button
+        onClick={() => {
+          message.success('This is a success message');
+        }}
+      >
+        Success
+      </Button>
+      <Button
+        onClick={() => {
+          message.error('This is an error message');
+        }}
+      >
+        Error
+      </Button>
+      <Button
+        onClick={() => {
+          message.warning('This is a warning message');
+        }}
+      >
+        Warning
+      </Button>
+      <Button
+        onClick={() => {
+          message.loading('This is a loading message');
+        }}
+      >
+        Loading
+      </Button>
+    </Space>
   );
 };
