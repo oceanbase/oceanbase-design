e8c0e082ae4aefa588bf4b5176477eda4f312762
dengfuping <fuping.dfp@antgroup.com>
2026-01-04 16:36:12 +0800

    improve(design): Alert mini style

--- packages/design/src/alert/demo/mini.tsx
+++ packages/design/src/alert/demo/mini.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 
 const App: React.FC = () => (
   <Space direction="vertical" style={{ width: '100%' }}>
-    <Alert message="Info Text" type="info" showIcon mini />
-    <Alert message="Success Text" type="success" showIcon mini />
-    <Alert message="Warning Text" type="warning" showIcon mini />
-    <Alert message="Error Text" type="error" showIcon mini />
+    <Alert message="Info Text" type="info" showIcon mini closable />
+    <Alert message="Success Text" type="success" showIcon mini closable />
+    <Alert message="Warning Text" type="warning" showIcon mini closable />
+    <Alert message="Error Text" type="error" showIcon mini closable />
   </Space>
 );
 
