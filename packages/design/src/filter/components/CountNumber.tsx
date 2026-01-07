49c481b6a94838f1d2675792452ba8776026738a
Richard Zhang <aaaayu1019@gmail.com>
2026-01-07 15:36:29 +0800

    fix(filter): style

--- packages/design/src/filter/components/CountNumber.tsx
+++ packages/design/src/filter/components/CountNumber.tsx
@@ -12,24 +12,13 @@ const CountNumber: FC<CountNumberProps> = ({ count, total, className }) => {
   const { prefixCls } = useFilterStyle();
 
   return (
-    <div className={classNames(className || getFilterCls(prefixCls, 'count'))}>
+    <div
+      className={classNames(className || getFilterCls(prefixCls, 'count'))}
+      style={total ? { width: '26px' } : undefined}
+    >
       {total ? `${count}/${total}` : count}
     </div>
   );
-  // const { token } = theme.useToken();
-  // return count > 0 ? (
-  //   <Badge
-  //     color={token.colorFillSecondary}
-  //     count={total ? `${count}/${total}` : `${count}`}
-  //     styles={{
-  //       indicator: {
-  //         borderColor: token.colorFillSecondary,
-  //         color: token.colorTextSecondary,
-  //       },
-  //     }}
-  //     className={className}
-  //   />
-  // ) : null;
 };
 
 export default CountNumber;
