b11a24e3f855b1e31faedba8b71b7c5e8a49df7c
dengfuping <dengfuping@users.noreply.github.com>
2026-04-28 12:53:04 +0000

    deploy: a092545b4983aaad3b9682e4555ad2ddb25a3d54

---
+++ docs__design__design-theme.md.4564bce8.async.js
@@ -0,0 +1,50 @@
+"use strict";(self.webpackChunkroot=self.webpackChunkroot||[]).push([[8424],{78528:function(i,t,e){e.r(t);var s=e(31497),m=e(21436),l=e(87373),c=e(51845),E=e(29846),a=e(96307),g=e(2444),h=e(91309),p=e(55931),D=e(87884),v=e(75861),x=e(10789),P=e(36946),I=e(91116),b=e(82490),O=e(46927),C=e(36402),M=e(27402),Z=e(85597),f=e(43661),o=e(40191),r=e(22110),d=e(75271),n=e(53743),_=e(56517);function u(){return(0,_.tZ)(o.dY,null,(0,_.tZ)(d.Suspense,{fallback:(0,_.tZ)(r.Z,null)},(0,_.tZ)(d.Fragment,null,(0,_.tZ)("div",{className:"markdown"},(0,_.tZ)("p",null,n.texts[0].value),(0,_.tZ)("h2",{id:"\u4E3B\u9898\u914D\u7F6E"},(0,_.tZ)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u4E3B\u9898\u914D\u7F6E"},(0,_.tZ)("span",{className:"icon icon-link"})),"\u4E3B\u9898\u914D\u7F6E"),(0,_.tZ)(a.Z,{lang:"tsx"},n.texts[1].value),(0,_.tZ)("h2",{id:"\u5185\u7F6E\u4E3B\u9898"},(0,_.tZ)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u5185\u7F6E\u4E3B\u9898"},(0,_.tZ)("span",{className:"icon icon-link"})),"\u5185\u7F6E\u4E3B\u9898"),(0,_.tZ)("h3",{id:"\u6697\u8272\u4E3B\u9898"},(0,_.tZ)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u6697\u8272\u4E3B\u9898"},(0,_.tZ)("span",{className:"icon icon-link"})),"\u6697\u8272\u4E3B\u9898"),(0,_.tZ)(a.Z,{lang:"ts"},n.texts[2].value),(0,_.tZ)("h3",{id:"\u7D27\u51D1\u4E3B\u9898"},(0,_.tZ)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u7D27\u51D1\u4E3B\u9898"},(0,_.tZ)("span",{className:"icon icon-link"})),"\u7D27\u51D1\u4E3B\u9898"),(0,_.tZ)(a.Z,{lang:"ts"},n.texts[3].value),(0,_.tZ)("h3",{id:"\u963F\u91CC\u4E91\u4E3B\u9898"},(0,_.tZ)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u963F\u91CC\u4E91\u4E3B\u9898"},(0,_.tZ)("span",{className:"icon icon-link"})),"\u963F\u91CC\u4E91\u4E3B\u9898"),(0,_.tZ)(a.Z,{lang:"ts"},n.texts[4].value),(0,_.tZ)("h3",{id:"\u4E3B\u9898\u9884\u89C8"},(0,_.tZ)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u4E3B\u9898\u9884\u89C8"},(0,_.tZ)("span",{className:"icon icon-link"})),"\u4E3B\u9898\u9884\u89C8")),(0,_.tZ)(o.Dl,{demo:{id:"docs-design-design-theme-demo-previewer"},previewerProps:{transform:!0,compact:!0,filename:"packages/design/src/theme/demo/previewer.tsx"}}),(0,_.tZ)("div",{className:"markdown"},(0,_.tZ)("h2",{id:"\u4F7F\u7528-design-token"},(0,_.tZ)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u4F7F\u7528-design-token"},(0,_.tZ)("span",{className:"icon icon-link"})),"\u4F7F\u7528 Design Token"),(0,_.tZ)("ul",null,(0,_.tZ)("li",null,n.texts[5].value,(0,_.tZ)(o.rU,{to:"/docs/design-token"},n.texts[6].value),n.texts[7].value)),(0,_.tZ)("h2",{id:"\u4F7F\u7528-design-token-\u65E7\u7248"},(0,_.tZ)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u4F7F\u7528-design-token-\u65E7\u7248"},(0,_.tZ)("span",{className:"icon icon-link"})),"\u4F7F\u7528 Design Token (\u65E7\u7248)"),(0,_.tZ)("ul",null,(0,_.tZ)("li",null,n.texts[8].value,(0,_.tZ)(o.rU,{to:"/docs/design-token-legacy"},n.texts[9].value),n.texts[10].value))))))}t.default=u},53743:function(i,t,e){e.r(t),e.d(t,{texts:function(){return s}});const s=[{value:"OceanBase Design \u7684\u8BBE\u8BA1\u4F53\u7CFB\u9075\u5FAA Ant Design 5.0 \u8BBE\u8BA1\u89C4\u8303\uFF0C\u5E76\u5728\u6B64\u57FA\u7840\u4E0A\u6269\u5C55\u51FA\u4E86\u5F88\u591A\u5177\u5907 OceanBase \u4EA7\u54C1\u98CE\u683C\u7684\u8BBE\u8BA1\u89C4\u8303\u6A21\u5F0F\uFF0C\u5305\u62EC\u4F46\u4E0D\u9650\u4E8E\u5168\u5C40\u6837\u5F0F\uFF08\u8272\u677F\u3001\u5706\u89D2\u3001\u8FB9\u6846\uFF09\u548C\u7279\u5B9A\u7EC4\u4EF6\u7684\u89C6\u89C9\u5B9A\u5236\uFF0C\u4EE5\u4F20\u9012 OceanBase \u79D1\u6280\u3001\u6D3B\u529B\u3001\u4E13\u6CE8\u548C\u5173\u6000\u7684\u54C1\u724C\u7279\u70B9\u3002",paraId:0},{value:`import { ConfigProvider } from '@oceanbase/design';
+
+export default () {
+  return (
+    <ConfigProvider theme={{ xxx }}>
+      {...}
+    </ConfigProvider>
+  );
+};
+`,paraId:1,tocIndex:0},{value:`import {  ConfigProvider } from '@oceanbase/design';
+
+export default () {
+  return (
+    <ConfigProvider
+      theme={{
+        isDark: true,
+        algorithm: theme.darkAlgorithm,
+      }}
+    >
+      {...}
+    </ConfigProvider>
+  );
+};
+`,paraId:2,tocIndex:2},{value:`import {  ConfigProvider } from '@oceanbase/design';
+
+export default () {
+  return (
+    <ConfigProvider
+      theme={{
+        algorithm: theme.compactAlgorithm
+      }}
+    >
+      {...}
+    </ConfigProvider>
+  );
+};
+`,paraId:3,tocIndex:3},{value:`import {  ConfigProvider } from '@oceanbase/design';
+
+export default () {
+  return (
+    <ConfigProvider
+      theme={{
+        isAliyun: true,
+      }}
+    >
+      {...}
+    </ConfigProvider>
+  );
+};
+`,paraId:4,tocIndex:4},{value:"\u8BF7\u53C2\u8003 ",paraId:5,tocIndex:6},{value:"Design Token",paraId:6,tocIndex:6},{value:"\u3002",paraId:5,tocIndex:6},{value:"\u8BF7\u53C2\u8003 ",paraId:7,tocIndex:7},{value:"Design Token (\u65E7\u7248)",paraId:8,tocIndex:7},{value:"\u3002",paraId:7,tocIndex:7}]}}]);
