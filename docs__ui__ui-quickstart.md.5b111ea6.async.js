30a2a5e6bae8b3610383c51c35ff556fc648d8c2
dengfuping <dengfuping@users.noreply.github.com>
2026-05-19 09:50:25 +0000

    deploy: fedb8410d162e2dc88870d30fd25baaa3d6fd5e0

---
+++ docs__ui__ui-quickstart.md.5b111ea6.async.js
@@ -0,0 +1,14 @@
+"use strict";(self.webpackChunkroot=self.webpackChunkroot||[]).push([[6824],{43217:function(d,n,_){_.r(n);var a=_(31497),m=_(21436),l=_(87373),c=_(51845),E=_(29846),s=_(96307),h=_(2444),p=_(91309),b=_(55931),g=_(87884),D=_(75861),x=_(10789),P=_(36946),O=_(91116),M=_(82490),v=_(46927),I=_(36402),C=_(27402),B=_(85597),y=_(43661),i=_(40191),r=_(22110),o=_(75271),t=_(59044),e=_(56517);function u(){return(0,e.tZ)(i.dY,null,(0,e.tZ)(o.Suspense,{fallback:(0,e.tZ)(r.Z,null)},(0,e.tZ)(o.Fragment,null,(0,e.tZ)("div",{className:"markdown"},(0,e.tZ)("h2",{id:"\u4F7F\u7528-codesandbox-\u5FEB\u901F\u521B\u5EFA"},(0,e.tZ)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u4F7F\u7528-codesandbox-\u5FEB\u901F\u521B\u5EFA"},(0,e.tZ)("span",{className:"icon icon-link"})),"\u4F7F\u7528 CodeSandbox \u5FEB\u901F\u521B\u5EFA"),(0,e.tZ)("ul",null,(0,e.tZ)("li",null,t.texts[0].value,(0,e.tZ)("a",{href:"https://codesandbox.io/s/oceanbase-ui-reproduction-template-cknd9y",target:"_blank"},t.texts[1].value),t.texts[2].value)),(0,e.tZ)("iframe",{src:"https://codesandbox.io/embed/oceanbase-ui-reproduction-template-cknd9y?fontsize=14&hidenavigation=1&theme=dark",style:{width:"100%",height:"500px",border:"0",borderRadius:"4px",overflow:"hidden"},title:"@oceanbase/ui reproduction template",allow:"accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking",sandbox:"allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"}),(0,e.tZ)("h2",{id:"\u5728\u9879\u76EE\u4E2D\u4F7F\u7528"},(0,e.tZ)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u5728\u9879\u76EE\u4E2D\u4F7F\u7528"},(0,e.tZ)("span",{className:"icon icon-link"})),"\u5728\u9879\u76EE\u4E2D\u4F7F\u7528"),(0,e.tZ)("h3",{id:"\u5B89\u88C5"},(0,e.tZ)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u5B89\u88C5"},(0,e.tZ)("span",{className:"icon icon-link"})),"\u5B89\u88C5"),(0,e.tZ)(s.Z,{lang:"bash"},t.texts[3].value),(0,e.tZ)("h3",{id:"\u793A\u4F8B"},(0,e.tZ)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u793A\u4F8B"},(0,e.tZ)("span",{className:"icon icon-link"})),"\u793A\u4F8B"),(0,e.tZ)(s.Z,{lang:"jsx"},t.texts[4].value)))))}n.default=u},59044:function(d,n,_){_.r(n),_.d(n,{texts:function(){return a}});const a=[{value:"\u53EF\u4EE5\u4F7F\u7528\u6211\u4EEC\u63D0\u4F9B\u7684 ",paraId:0,tocIndex:0},{value:"CodeSandbox \u6A21\u677F",paraId:0,tocIndex:0},{value:" \u5FEB\u901F\u521B\u5EFA\u548C\u9884\u89C8\u3002",paraId:0,tocIndex:0},{value:`$ npm i @oceanbase/ui --save
+`,paraId:1,tocIndex:2},{value:`import { Lottie } from '@oceanbase/ui';
+
+const App = () => {
+  return (
+    <Lottie
+      path="https://assets9.lottiefiles.com/packages/lf20_WPqksadnNs.json"
+      style={{
+        height: 200,
+      }}
+    />
+  );
+};
+`,paraId:2,tocIndex:3}]}}]);
