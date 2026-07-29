14e3b97c8241d7120a46a824aba752a3da29d34f
dengfuping <fuping.dfp@antgroup.com>
2026-07-29 14:17:48 +0800

    feat(design): implement Input.Search with prefix icon on InternalInput

--- packages/design/src/input/Search.tsx
+++ packages/design/src/input/Search.tsx
@@ -1,11 +1,14 @@
-import React, { forwardRef, useContext } from 'react';
+import React, { forwardRef, useContext, useRef } from 'react';
+import classNames from 'classnames';
 import { Input as AntInput } from 'antd';
+import { SearchOutlined } from '@oceanbase/icons';
+import { composeRef } from 'rc-util/lib/ref';
 import type { SearchProps as AntSearchProps } from 'antd/es/input/Search';
-import type { InputLocale, InputRef } from './Input';
 import ConfigProvider from '../config-provider';
 import type { ConfigConsumerProps } from '../config-provider';
 import defaultLocale from '../locale/en-US';
-import { showCountFormatter } from './Input';
+import InternalInput, { showCountFormatter } from './Input';
+import type { InputLocale, InputRef } from './Input';
 import useStyle from './style';
 
 export * from 'antd/es/input/Search';
@@ -14,32 +17,106 @@ export interface SearchProps extends AntSearchProps {
   locale?: InputLocale;
 }
 
-const Search = forwardRef<InputRef, SearchProps>(
-  ({ prefixCls: customizePrefixCls, locale: customLocale, showCount, ...restProps }, ref) => {
-    const { getPrefixCls, locale: contextLocale } = useContext<ConfigConsumerProps>(
-      ConfigProvider.ConfigContext
-    );
-    const prefixCls = getPrefixCls('input', customizePrefixCls);
-    const [wrapCSSVar] = useStyle(prefixCls);
-    const inputLocale: InputLocale = {
-      placeholder:
-        contextLocale?.global?.inputPlaceholder || defaultLocale.global?.inputPlaceholder,
-      ...defaultLocale.Input,
-      ...contextLocale?.Input,
-      ...customLocale,
+const Search = forwardRef<InputRef, SearchProps>((props, ref) => {
+  const {
+    prefixCls: customizePrefixCls,
+    inputPrefixCls: customizeInputPrefixCls,
+    className,
+    enterButton = false,
+    addonAfter,
+    prefix,
+    locale: customLocale,
+    showCount,
+    onSearch,
+    onChange: onChangeProp,
+    onPressEnter: onPressEnterProp,
+    onCompositionStart,
+    onCompositionEnd,
+    loading,
+    ...restProps
+  } = props;
+
+  const {
+    getPrefixCls,
+    locale: contextLocale,
+    direction,
+  } = useContext<ConfigConsumerProps>(ConfigProvider.ConfigContext);
+  const prefixCls = getPrefixCls('input-search', customizePrefixCls);
+  const inputPrefixCls = getPrefixCls('input', customizeInputPrefixCls);
+  const [wrapCSSVar] = useStyle(inputPrefixCls);
+  const searchPrefix = prefix ?? <SearchOutlined />;
+  const inputRef = useRef<InputRef>(null);
+  const composedRef = useRef(false);
+
+  const inputLocale: InputLocale = {
+    placeholder: contextLocale?.global?.inputPlaceholder || defaultLocale.global?.inputPlaceholder,
+    ...defaultLocale.Input,
+    ...contextLocale?.Input,
+    ...customLocale,
+  };
+
+  if (!enterButton && !addonAfter) {
+    const handleChange: AntSearchProps['onChange'] = e => {
+      if (e?.target && e.type === 'click' && onSearch) {
+        onSearch(e.target.value, e, { source: 'clear' });
+      }
+      onChangeProp?.(e);
     };
 
-    return wrapCSSVar(
-      <AntInput.Search
-        ref={ref}
-        prefixCls={customizePrefixCls}
-        placeholder={inputLocale.placeholder}
-        showCount={showCount === true ? { formatter: showCountFormatter } : showCount}
+    const handlePressEnter: AntSearchProps['onPressEnter'] = e => {
+      if (composedRef.current || loading) {
+        return;
+      }
+      onPressEnterProp?.(e);
+      onSearch?.(inputRef.current?.input?.value ?? '', e, { source: 'input' });
+    };
+
+    const cls = classNames(prefixCls, { [`${prefixCls}-rtl`]: direction === 'rtl' }, className);
+
+    return (
+      <InternalInput
+        ref={composeRef(inputRef, ref)}
+        prefixCls={customizeInputPrefixCls}
+        locale={customLocale}
+        showCount={showCount}
         {...restProps}
+        className={cls}
+        type="search"
+        prefix={searchPrefix}
+        onChange={handleChange}
+        onPressEnter={handlePressEnter}
+        onCompositionStart={e => {
+          composedRef.current = true;
+          onCompositionStart?.(e);
+        }}
+        onCompositionEnd={e => {
+          composedRef.current = false;
+          onCompositionEnd?.(e);
+        }}
       />
     );
   }
-);
+
+  return wrapCSSVar(
+    <AntInput.Search
+      ref={ref}
+      prefixCls={customizeInputPrefixCls}
+      className={classNames(prefixCls, className)}
+      placeholder={inputLocale.placeholder}
+      showCount={showCount === true ? { formatter: showCountFormatter } : showCount}
+      prefix={searchPrefix}
+      enterButton={enterButton}
+      addonAfter={addonAfter}
+      loading={loading}
+      onSearch={onSearch}
+      onChange={onChangeProp}
+      onPressEnter={onPressEnterProp}
+      onCompositionStart={onCompositionStart}
+      onCompositionEnd={onCompositionEnd}
+      {...restProps}
+    />
+  );
+});
 
 if (process.env.NODE_ENV !== 'production') {
   Search.displayName = 'Search';
